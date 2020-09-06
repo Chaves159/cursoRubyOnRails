ff1b453e69351f52acea66aa0803445cc7ccf276
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-06 20:30:40 -0300

    Trabalhando com os simbolos em Ruby

---
+++ Modulo_2/symbols.rb
@@ -0,0 +1,21 @@
+puts "Joao".object_id
+puts "Joao".object_id
+puts "Joao".object_id
+puts "Joao".object_id
+
+
+puts "===================="
+
+puts :joao.object_id
+puts :joao.object_id
+puts :joao.object_id
+puts :joao.object_id
+
+
+puts "===================="
+
+
+NOME = "João Victor Chaves"
+
+puts NOME
+
