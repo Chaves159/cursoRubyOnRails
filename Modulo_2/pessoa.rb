3ba3cb5cea64bb8e153f1ca7f43d51e36b9de84a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-06 20:54:08 -0300

    Implementando metodos

--- Modulo_2/pessoa.rb
+++ Modulo_2/pessoa.rb
@@ -1,5 +1,13 @@
-class Pessoa
-	def falar
-		"Ola, pessoal!"
+class Pessoa 
+	def falar (nome)
+		"Ola, #{nome}!"
+	end
+	def initialize (cont = 1)
+		cont.times do |i|
+			puts "Inicializando... #{i}"
+		end
 	end
 end
+
+p = Pessoa.new(10)
+puts p.falar("João Victor Chaves")
