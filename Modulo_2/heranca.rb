074f5eff18aabbcab80f1b7f3c2d3fe6f6e73818
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-06 21:41:24 -0300

    Implementando metodos nas classes

---
+++ Modulo_2/heranca.rb
@@ -0,0 +1,67 @@
+class Pessoa
+	attr_accessor :nome, :email
+end
+
+class PessoaFisica < Pessoa
+	attr_accessor :cpf
+
+	def falar(texto)
+		texto
+	end
+end
+
+class PessoaJuridica < Pessoa
+	attr_accessor :cnpj
+
+	def pagar_fornecedor
+		puts "Pagando Fornecedor"
+	end
+end
+
+
+
+ p1 = Pessoa.new
+
+#SETTER
+ p1.nome = "João"
+ p1.email = "joao.victor.chaves06@aluno.ifce.edu.br"
+
+#getter
+
+puts p1.nome
+ puts p1.email
+
+puts "========================================"
+ p2 = PessoaFisica.new
+#SETTER
+ p2.nome = "victor"
+
+ p2.email = "joao.victor.chaves159@gmail.com"
+
+ p2.cpf = "159.000.357-06"
+
+#getter
+ puts p2.nome
+ puts p2.email
+ puts p2.cpf
+ puts p2.falar("Olá, Mundo")
+puts "========================================"
+ p3 = PessoaJuridica.new
+#SETTER
+ p3.nome = "Chaves"
+
+ p3.email = "joao.victor.chaves159@gmail.com"
+
+ p3.cnpj = "000.000.000-06"
+
+ p3.pagar_fornecedor
+
+#getter
+
+puts p3.nome
+puts p3.email
+puts p3.cnpj
+puts p3.pagar_fornecedor
+puts "========================================"
+
+ 
---
+++ Modulo_2/instancia.rb
@@ -0,0 +1,18 @@
+class Pessoa
+	def initialize(nome_fornecido = "Indigente")
+		@nome = nome_fornecido
+	end
+
+	def imprimir_nome
+		@nome
+	end
+end
+
+p1 = Pessoa.new
+
+puts p1.imprimir_nome
+
+p2 = Pessoa.new("João Victor Chaves")
+
+puts p2.imprimir_nome
+
