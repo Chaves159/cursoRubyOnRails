722c689f094e8f917e274fa73b5f7e6d5a9ae21a
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-14 15:20:02 -0300

    Refatorando a Seeds

--- Modulo_3/crypo_wallet/db/seeds.rb
+++ Modulo_3/crypo_wallet/db/seeds.rb
@@ -5,18 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://img2.gratispng.com/20180517/rbq/kisspng-bitcoin-cryptocurrency-exchange-digital-currency-t-5afdf7b0771a30.4144674615265934564879.jpg"
-)
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/471px-Ethereum_logo_2014.svg.png"
-)
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://image.shutterstock.com/image-illustration/raster-illustration-dash-crypto-currency-260nw-1009072462.jpg"
-)
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas... ")
+spinner.auto_spin
+coins =
+    [
+        {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://img2.gratispng.com/20180517/rbq/kisspng-bitcoin-cryptocurrency-exchange-digital-currency-t-5afdf7b0771a30.4144674615265934564879.jpg"
+        },
+        {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/471px-Ethereum_logo_2014.svg.png"
+        },
+        {
+            description: "Dash",
+            acronym: "DASH",
+            url_image: "https://image.shutterstock.com/image-illustration/raster-illustration-dash-crypto-currency-260nw-1009072462.jpg"
+        }
+    ]
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+spinner.success("Concluido!")
