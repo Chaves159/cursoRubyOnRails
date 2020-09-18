6d6f4b84ae5b03c67f180497e795a5a3855ef3f1
João Victor Chaves <joao.victor.chaves159@gmail.com>
2020-09-18 14:59:24 -0300

    Manipulando cookies e sessions

--- Modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
+++ Modulo_3/crypo_wallet/app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIES]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
     @nome = params[:nome]
     @curso = params[:curso]
   end
